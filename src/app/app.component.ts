15442262727e84d807c7cdfbf525e73f562de17b
Maksim_Perakladau <maksim.perakladau@crealogix.com>
2022-09-26 16:57:12 +0300

    Add carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'polyclinicAngular';
+  public slides: Array<any>= [
+    { src: "assets/images/amsterdamslide__800x600.jpg" },
+    { src: "assets/images/foyleswarslide__800x600.jpg" },
+    { src: "assets/images/lands-endslide__800x600.jpg" },
+    { src: "assets/images/trustslide-2__800x600.jpg" }
+  ];
 }
--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-carousel',
+  selector: 'carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+  @Input()
+  slides: Array<any> = [];
+
+  currentSlide: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onPreviousClick() {
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
+    console.log("previous clicked, new current slide is: ", this.currentSlide);
+  }
+
+  onNextClick() {
+    const next = this.currentSlide + 1;
+    this.currentSlide = next === this.slides.length ? 0 : next;
+    console.log("next clicked, new current slide is: ", this.currentSlide);
   }
 
 }
